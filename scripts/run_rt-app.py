7bdd958d179f2352b0724732fe9d543a227c6f01
Insop Song <insop.song@gmail.com>
2013-03-20 00:41:26 -0700

    script update

--- scripts/run_rt-app.py
+++ scripts/run_rt-app.py
@@ -3,12 +3,12 @@
 #
 # note:
 #
-# 0. install fabric 1.6 on the develop host
+# 0. install fabric 1.6 or higher on the develop host
 # - $ pip install fabric
 #
 # 1. copy ssh public key (copy and paste host's "~/.ssh/id_rsa.pub" to the remote's ~/.ssh/authorized_keys)
 #
-# 2. update the "env.hosts" with your remote system
+# 2. update the below "env.hosts" with your remote system
 #
 # 3. run the script from the host
 # - $ fab -f run_rt-app.py run_app
@@ -26,6 +26,7 @@ import time
 import datetime
 
 
+# remote target address and id
 env.hosts = ['root@192.168.1.78']
 remote_ip = env.hosts[0]
 
@@ -40,12 +41,13 @@ RTAPP	= "/usr/bin/rt-app"
 period_t	= 100000
 run_t		= 30000
 
-num_run = 10
-sec_run = 100
+num_run = 20
+sec_run = 60
 
 dir_name	= "/home/root"
-f_name		= "rt-app_run_log-%s.txt" % now.strftime("%Y-%m-%d-%H-%M")
-file_name	= "%s/%s" % (dir_name, f_name)
+file_name	= "rt-app_run_log-%s.txt" % now.strftime("%Y-%m-%d-%H-%M")
+
+log_file	= "%s/%s" % (dir_name, file_name)
 	
 def pre():
 	print "\n==========================================================="
@@ -65,21 +67,23 @@ def main():
 	print "2. Running rt-app %s times.." % num_run
 	print "===========================================================\n"
 
+	# running rt-app on the remote target and collect data
 	run('echo \"test log (period = %s, execution time %s) run %s times on each %s sec \n\n\" > %s' % \
-		(period_t, run_t, num_run, sec_run, file_name))
+		(period_t, run_t, num_run, sec_run, log_file))
 	for i in range(0, num_run):
 		run("%s -t %s:%s:d -D %s >> %s" %  \
-			(RTAPP, period_t, run_t, sec_run, file_name))
+			(RTAPP, period_t, run_t, sec_run, log_file))
 
 #
 # bring the data log to the host
 #
 def post():
 	# running this from local host
-	local("%s %s:%s ." % (SCP, remote_ip, file_name))
+	# scp-ing log file from remote to local host
+	local("%s %s:%s ." % (SCP, remote_ip, log_file))
 
 	print "\n=============================================================================="
-	print "3. Run finished, and log file ""%s"" is copied to host." % f_name
+	print "3. Run finished, and log file ""%s"" is copied to host." % file_name
 	print "==============================================================================\n"
 
 def run_app():
